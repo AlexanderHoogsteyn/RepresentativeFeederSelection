c697a8172698bb3be05519e1881ab1a6b1427c79
AlexanderHoogsteyn <r0652378@esat.kuleuven.be>
2020-09-14 16:57:01 +0200

    Julia parser completed

---
+++ parsers/README.md
@@ -0,0 +1,48 @@
+# createPowerModelDistributionFeeder.jl
+Script that contains functionalities to use GridDataCollections in PowerModelsDistribution. This makes it possible to do powerflow calculations on feeders in distribution networks. To do calculations in PowerModelsDistribution the network needs to be in a Julia Dictonary in specific format (more info on this format: https://lanl-ansi.github.io/PowerModelsDistribution.jl/latest/math-model/). Using this script, time series analysis can be performed (which is currently not natively supported in PowerDistributionModels, as of sept 2020).
+
+## Obtaining GridDataCollections
+This scripts imports the network topology in specific format in JSON files. Such a representation of a distribution network can be obtained using a script
+such as createGridDataCollection_dataframes_poly.py. That script in specific does
+the conversion from excel files as they were recieved by the Spanish DSO. Each feeder
+has it's information stored in 4 files:
+- Configuration: contains general information and lists the directory's of the other 3 files.
+- Devices: contains information about the devices i.e. customers connected. For example the EAN-number and connection capacity. The total yearly consumption (kWh) is included, in createGridDataCollection_dataframes_poly.py this is estimated based of 20 days of smart meter data. The phase connection is as for now, randomly allocated.
+- Branches: contains information on cable length, bus connections and type of each branch. The latter is used to determine the impedance matrix
+- Buses: contains info on which bus is the slack bus and voltage limits on each bus. The voltage limits are added to model but are are only used if optimal powerflow calculations are performed. This script is however not supposed to be used for optimal powerflow calculations. If this is of interest, additional fields will have to be added to the data model.
+The .JSON files should be in a folder, the directory of the folder should be specified on top of the script. As well as the configuration file of the feeder to be analyzed.
+```Julia
+dir = "C:Home/folder/"
+config_file_name = "CITY/feeder-name_configuration.json"
+```
+
+## Building PowerModelsDistribution MATHEMATICAL model
+If a powerflow calculation is required of a single instance in time the function
+build_mathematical_model() can be used. Three phase active & reactive power
+for all devices in the network can be set using pd & qd respectively (in pu)
+```Julia
+network = build_mathematical_model(dir, config_file_name)
+solver = with_optimizer(Ipopt.Optimizer, print_level = 0, tol=1e-9)
+result = run_mc_pf(network,,ACPPowerModel,solver)
+```
+### Limitations
+- The impedance matrix is diagonal for each branch since of each cable type only the R/km and X/km were known. Mutual Impedance is not accounted for.
+- R/km and X/km are hard coded in the script for the specific cable types in the Spanish data set. If other cable types are used this needs adaption.
+- The impedance that the transformer at the head of the feeder adds, is not accounted for.
+- Ground admittance is not accounted for
+
+## Time series analysis
+To perform a time series analysis, load profiles should be estimated and assigned to each device i.e. customer. Load profiles can be imported from csv files using read_reference_profile_from_csv() or using read_dublin_data(). The resulting data format is an array of samples of used energy (kWh). Or in case of read_dublin_data() an array of array's with samples of used energy. This needs to be passed on as argument to build_mn_mathematical_model(). The time interval of the samples (expressed in hours) should also be passed, this is needed to convert energy to power. If a load profile with instantaneous power samples is used, set the time interval to 1. A scale factor can also be applied to the load profile.
+```Julia
+referenc_profiles = read_dublin_data()
+time_steps = 48 #one day
+multinetwork = build_mn_mathematical_model(dir, config_file_name, reference_profiles, time_steps, scale_factor=1.0, time_unit=0.5)
+result = run_mn_mc_pf(multinetwork,ACPPowerModel,solver)
+```
+In PowerModels, the package where PowerModelsDistribution is based on, there is support for multinetworks which are networks that change for example their load over time i.e. it is possible to do time series analyses.
+run_mn_mc_pf() will perform a multinetwork variant of the multiconductor powerflow problem (Thus taking into account phase inbalances). This is currently not natively supported in PowerModelsDistribution but this script includes the functions needed to build a multinetwork formulation of the powerflow problem
+
+### Limitaions
+- Devices that have a three phase connection have their power equally divided over the three phases
+- The network topology is fixed during the time series analysis. Influence of tap changes or switches in the topology are not possible in its current form.
+- Writing back the solution takes a lot of time, this mitigates the advantage of using a multinetwork variant. On some machines it can be faster to use naive_time_series_analysis() instead. This implementation just performs a power flow calculation for each time step.   
--- parsers/createPowerDistributionModel.jl
+++ parsers/createPowerDistributionModel.jl
@@ -2,27 +2,38 @@ using JSON
 using DataFrames
 using CSV
 using PowerModelsDistribution
+using PowerModels
 using Ipopt
 using Statistics
-
+using Plots
+"""
+Parser for distribution network model in .JSON format to
+PowerModelsDistribution MATHEMATICAL formultation (more info on this format:
+https://lanl-ansi.github.io/PowerModelsDistribution.jl/latest/math-model/).
+Time series analysis can be performed (which is currently not natively supported
+in PowerDistributionModels, as of sept 2020)
+.JSON files should be in a folder, the directory of the folder should be specified below
+the configuration file of the feeder to be analyzed as well
+Date: 16 sept 2020
+Author: Alexander Hoogsteyn
+"""
 dir = "C:/Users/AlexH/OneDrive/Documenten/Julia/Implementation of network + clustering of network feeders/summer job Alexander/"
 feeder = "POLA/65019_74469_configuration.json"
 
+"""
+Specify voltage and power base, voltage base should be the phase-to-ground voltage
+of the feeder studied (kV), the power base can be arbitrairly chosen (MW)
+"""
 const voltage_base = 0.230  # (kV)
 const power_base = 0.5  # (MW)
 const Z_base = voltage_base^2/power_base # (Ohm)
 
-function build_mathematical_network_model(dir, config_file_name, time_step,reference_profiles,scale_factor=1.0)
-    #
-    #   Function that builds a network model in the PowerModelsDistribution format
-    #   (mathematical) from a JSON file name & location
-    #   Additionaly, it needs reference load profiles to attach time series data to the customers
-    #   reference_profiles should by a Array that contains kWh use for each
-    #   half hour for an entire year. Optionally, a scaling factor for the profile can be given.
-    #   Optionally, the reference_profiles can be a mulidimentional array which contains a
-    #   series of load profile Arrays, then a profile that is closest to the customer
-    #   is selected (based on the total consumption)
-    #
+"""
+Function that builds a network model in the PowerModelsDistribution format
+(mathematical) from a JSON file name & location. Three phase active & reactive power
+for all devices in the network can be set using pd & qd respectively (in pu).
+"""
+function build_mathematical_model(dir, config_file_name,pd=[0.0, 0.0, 0.0],qd=[0.0, 0.0, 0.0],scale_factor=1.0)
     configuration = "star"
     network_model = Dict{String,Any}()
     configuration_json_dict = Dict{Any,Any}()
@@ -103,7 +114,7 @@ function build_mathematical_network_model(dir, config_file_name, time_step,refer
                     "bus_i"     => id,
                     "vmin"      => [0.0, 0.0, 0.0],
                     "vmax"      => [1.5, 1.5, 1.5],
-                    "va"        => [0.0, -2.0944, -2.0944],
+                    "va"        => [0.0, 2.0944, -2.0944],
                     "vm"        => [1.0, 1.0, 1.0])
             else
                 network_model["bus"][id_s] = Dict{String,Any}(
@@ -139,32 +150,11 @@ function build_mathematical_network_model(dir, config_file_name, time_step,refer
             "dispatchable"  => 0,
             "index"         => id,
             "yearlyNetConsumption" => cons,
-            "phases"        => device["phases"]
+            "phases"        => device["phases"],
+            "pd"            => pd,
+            "qd"            => qd
         )
-        if eltype(reference_profiles) == Float64    #If no multidimensional array is given
-            reference_profiles = [reference_profiles]   #make it into one
         end;
-        load_profile = pick_load_profile(cons,reference_profiles)   #Pick the best fit load profile
-        load_profile = scale_factor*load_profile/500 #scale from kWh to MW
-        pd = load_profile[time_step]/ power_base #convert to per-uits
-        qd = pd/20 #Estimation for reactive power
-
-        if length(device["phases"]) == 3   #Three phase connection
-            network_model["load"][id_s]["pd"] = pd .* [0.33, 0.33, 0.33]
-            network_model["load"][id_s]["qd"] = qd .* [0.33, 0.33, 0.33]
-        elseif device["phases"][1] == 1   #Connected to phase 1
-            network_model["load"][id_s]["pd"] = pd .* [0.0, 1.0, 0.0]
-            network_model["load"][id_s]["qd"] = qd .* [0.0, 1.0, 0.0]
-        elseif device["phases"][1] == 2   #Connected to phase 2
-            network_model["load"][id_s]["pd"] = pd .* [1.0, 0.0, 0.0]
-            network_model["load"][id_s]["qd"] = qd .* [1.0, 0.0, 0.0]
-        elseif device["phases"][1] == 3   #Connected to phase 3
-            network_model["load"][id_s]["pd"] = pd .* [1.0, 0.0, 0.0]
-            network_model["load"][id_s]["qd"] = qd .* [1.0, 0.0, 0.0]
-        else
-            print(device["phases"] * " is an unknown phase connection")
-        end;
-    end;
     end;
 
     open(dir * branches_file_name,"r") do io
@@ -222,55 +212,25 @@ function build_mathematical_network_model(dir, config_file_name, time_step,refer
                 "c_rating_c"    => [0.8, 0.8, 0.8]
             )
             if haskey(impedance_dict,branch["cableType"])
-                network_model["branch"][id_s]["br_r"] = impedance_dict[branch["cableType"]][1] .* (branch["cableLength"]/1000+1E-3) ./  Z_base .* [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
-                network_model["branch"][id_s]["br_x"] = impedance_dict[branch["cableType"]][2] .* (branch["cableLength"]/1000+1E-3) ./  Z_base .* [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
+                network_model["branch"][id_s]["br_r"] = impedance_dict[branch["cableType"]][1] .* (branch["cableLength"]/1000+1E-6) ./  Z_base .* [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
+                network_model["branch"][id_s]["br_x"] = impedance_dict[branch["cableType"]][2] .* (branch["cableLength"]/1000+1E-6) ./  Z_base .* [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
             end;
         end;
     end;
     return network_model
 end;
 
-function update_mathematical_network_model!(network_model,time_step,reference_profiles,scale_factor)
-    updated_network_model = network_model
-    if eltype(reference_profiles) == Float64    #If no multidimensional array is given
-        reference_profiles = [reference_profiles]
-    end;   #make it into one
-        for (id_s,device) in network_model["load"]
-            load_profile = pick_load_profile(device["yearlyNetConsumption"],reference_profiles)   #Pick the best fit load profile
-            load_profile = scale_factor*load_profile/500 #scale from kWh to MW
-            pd = load_profile[time_step]/power_base #convert to per-uits
-            qd = pd/20 #Estimation for reactive power
-
-            if length(device["phases"]) == 3   #Three phase connection
-                updated_network_model["load"][id_s]["pd"] = pd .* [0.33, 0.33, 0.33]
-                updated_network_model["load"][id_s]["qd"] = qd .* [0.33, 0.33, 0.33]
-            elseif device["phases"][1] == 1   #Connected to phase 1
-                updated_network_model["load"][id_s]["pd"] = pd .* [0.0, 1.0, 0.0]
-                updated_network_model["load"][id_s]["qd"] = qd .* [0.0, 1.0, 0.0]
-            elseif device["phases"][1] == 2   #Connected to phase 2
-                updated_network_model["load"][id_s]["pd"] = pd .* [1.0, 0.0, 0.0]
-                updated_network_model["load"][id_s]["qd"] = qd .* [1.0, 0.0, 0.0]
-            elseif device["phases"][1] == 3   #Connected to phase 3
-                updated_network_model["load"][id_s]["pd"] = pd .* [1.0, 0.0, 0.0]
-                updated_network_model["load"][id_s]["qd"] = qd .* [1.0, 0.0, 0.0]
-            else
-                print(device["phases"] * " is an unknown phase connection")
-            end;
-        end;
-    return updated_network_model
-end;
-
-function pick_load_profile(yearly_consumption,reference_profiles)
-    #
-    # Returns an array containing an estimated load_profile based on the households
-    # yearly consumption. The array consists of 30min samples of the used Energy (kWh)
-    # for exactly one year (thus 17520 samples)
-    #
-    load_profile = zeros(Float64,48*365)
+"""
+Returns an array containing an estimated load_profile based on the households
+mean power consumption. The array whithin reference_profiles with the nearest
+mean to target_mean is chosen. Units of target_mean and samples within
+reference_profiles should be the same (ussualy kWh).
+"""
+function pick_load_profile(target_mean,reference_profiles)
+    load_profile = zeros(Float64,length(reference_profiles[1]))
     smallest_error = Inf
     for i in reference_profiles
-        mean_consumption = convert(Float64, mean(i)*48*365)
-        error = abs(yearly_consumption-mean_consumption)
+        error = abs(target_mean-mean(i))
         if error < smallest_error
             smallest_error = error
             load_profile  = i
@@ -279,15 +239,21 @@ function pick_load_profile(yearly_consumption,reference_profiles)
     return load_profile
 end;
 
+function read_reference_profile_from_csv()
+end;
+
+"""
+Reads out CSV files from dublin data set and returns it as reference profiles
+that can be used to form a multinetwork model
+"""
 function read_dublin_data()
     reference_profiles = []
-    for file in 1:6
+    for file in 2:2
         csv_name = dir*"DUBLIN/38_CER Electricity_Gas/File"*string(file)*".txt"
         df = DataFrame(CSV.File(csv_name,delim=" ",header=["id","time","power"]))
         data = unstack(df,:time,:id,:power)
         for i in propertynames(data)
             if findfirst(ismissing,data[1:17520,i]) == nothing
-                #mean_power = convert(Float64, mean(data[1:17520,i])*48*365)
                 push!(reference_profiles, data[1:17520,i])
             end;
         end;
@@ -298,26 +264,183 @@ function read_dublin_data()
     return reference_profiles
 end;
 
-function time_code(decoded_time)
-    half_hours = decoded_time % 48
-    days = decoded_time ÷ 48
-    return 100*days + half_hours
+"""
+Builds a multinetwork model in a format similar to the one used in PowerModels
+but extended to be able to account for phase inbalance such as in PowerModelsDistribution
+Additionaly, it needs reference load profiles to attach time series data to the customers
+reference_profiles should by a Array that contains kWh use for each
+half hour for an entire year. Optionally, a scaling factor for the profile can be given.
+Optionally, the reference_profiles can be a mulidimentional array which contains a
+series of load profile Arrays, then a profile that is closest to the customer
+is selected (based on the total consumption)
+"""
+function build_mn_mathematical_model(dir,feeder,reference_profiles,time_steps,scale_factor=1.0,time_unit=0.5)
+    if eltype(reference_profiles) == Float64    #If no multidimensional array is given
+        reference_profiles = [reference_profiles]   #make it into one
+    end;
+
+    network_model = build_mathematical_model(dir,feeder)
+    mn_model = replicate(network_model,time_steps,global_keys=Set{String}())
+
+    for (id_s,device) in network_model["load"]
+        mean_power = device["yearlyNetConsumption"]*time_unit/365/24 #Assuming yearlyNetConsumption contains total consumption of 365 days in kWh
+        load_profile = pick_load_profile(mean_power,reference_profiles)   #Pick the best fit load profile
+        load_profile = scale_factor*load_profile/1000/time_unit #scale from kWh to MW
+        load_profile = load_profile/power_base #convert to per-uits
+        for step in 1:time_steps
+            pd = load_profile[step]
+            qd = pd/20
+            if length(device["phases"]) == 3   #Three phase connection
+                mn_model["nw"]["$(step)"]["load"][id_s]["pd"] = pd .* [0.33, 0.33, 0.33]
+                mn_model["nw"]["$(step)"]["load"][id_s]["qd"] = qd .* [0.33, 0.33, 0.33]
+            elseif device["phases"][1] == 1   #Connected to phase 1
+                mn_model["nw"]["$(step)"]["load"][id_s]["pd"] = pd .* [0.0, 1.0, 0.0]
+                mn_model["nw"]["$(step)"]["load"][id_s]["qd"] = qd .* [0.0, 1.0, 0.0]
+            elseif device["phases"][1] == 2   #Connected to phase 2
+                mn_model["nw"]["$(step)"]["load"][id_s]["pd"] = pd .* [1.0, 0.0, 0.0]
+                mn_model["nw"]["$(step)"]["load"][id_s]["qd"] = qd .* [1.0, 0.0, 0.0]
+            elseif device["phases"][1] == 3   #Connected to phase 3
+                mn_model["nw"]["$(step)"]["load"][id_s]["pd"] = pd .* [0.0, 0.0, 1.0]
+                mn_model["nw"]["$(step)"]["load"][id_s]["qd"] = qd .* [0.0, 0.0, 1.0]
+            else
+                print(device["phases"] * " is an unknown phase connection")
+            end;
+        end;
+    end;
+    return mn_model
 end;
 
-function decode_time_code(time_code )
-    time = (time_code % 100)
-    day = (time_code ÷ 100) % 365
-
-    return day*48+time
+"""
+Alias for when a single array is passed instead of a multidimentional array
+"""
+function build_mn_mathematical_model(dir,feeder,reference_profiles::Array{Float64,1},kwargs...)
+    return build_mn_mathematical_model(dir,feeder,reference_profiles=[reference_profiles],kwargs...)
 end;
 
+"""
+Variant on build_mc_pf() from PowerModelsDistribution that allows you to build
+multinetworks, similar to how it is implemented in build_mn_pf() from PowerModels
+"""
+function build_mn_mc_pf(pm::AbstractPowerModel)
+    for (n, network) in nws(pm)
+        variable_mc_bus_voltage(pm, nw=n, bounded=false)
+        variable_mc_branch_power(pm,nw=n,bounded=false)
+        variable_mc_transformer_power(pm, nw=n, bounded=false)
+        variable_mc_gen_power_setpoint(pm, nw=n, bounded=false)
+        variable_mc_load_setpoint(pm, nw=n, bounded=false)
+        variable_mc_storage_power(pm, nw=n, bounded=false)
+        constraint_mc_model_voltage(pm,nw=n)
+        for (i,bus) in ref(pm, :ref_buses, nw=n)
+            @assert bus["bus_type"] == 3
+            constraint_mc_theta_ref(pm, i, nw=n)
+            constraint_mc_voltage_magnitude_only(pm, i, nw=n)
+        end;
+        # gens should be constrained before KCL, or Pd/Qd undefined
+        for id in ids(pm, :gen, nw=n)
+        constraint_mc_gen_setpoint(pm, id, nw=n)
+        end;
+        # loads should be constrained before KCL, or Pd/Qd undefined
+        for id in ids(pm, :load, nw=n)
+            constraint_mc_load_setpoint(pm, id, nw=n)
+        end;
+        for (i,bus) in ref(pm, :bus, nw=n)
+            constraint_mc_load_power_balance(pm, i, nw=n)
+            # PV Bus Constraints
+            if length(ref(pm, :bus_gens, i, nw=n)) > 0 && !(i in ids(pm,:ref_buses, nw=n))
+                # this assumes inactive generators are filtered out of bus_gens
+                @assert bus["bus_type"] == 2
 
+                constraint_mc_voltage_magnitude_only(pm, i, nw=n)
+                for j in ref(pm, :bus_gens, i, nw=n)
+                    constraint_mc_gen_power_setpoint_real(pm, j, nw=n)
+                end;
+            end;
+        end;
+        for i in ids(pm, :storage, nw=n)
+            constraint_storage_state(pm, i, nw=n)
+            constraint_storage_complementarity_nl(pm, i, nw=n)
+            constraint_mc_storage_losses(pm, i, nw=n)
+            constraint_mc_storage_thermal_limit(pm, i, nw=n)
+        end;
+        for i in ids(pm, :branch, nw=n)
+            constraint_mc_ohms_yt_from(pm, i, nw=n)
+            constraint_mc_ohms_yt_to(pm, i, nw=n)
+        end;
+        for i in ids(pm, :transformer, nw=n)
+            constraint_mc_transformer_power(pm, i, nw=n)
+        end;
+    end;
+end;
+"Alias of build_mn_mc_pf() for when IVR formultation of pf is used"
+function build_mn_mc_pf(pm::AbstractIVRModel)
+    for (n, network) in nws(pm)
+        # Variables
+        variable_mc_bus_voltage(pm, nw=n, bounded = false)
+        variable_mc_branch_current(pm, nw=n, bounded = false)
+        variable_mc_transformer_current(pm, nw=n, bounded = false)
+        variable_mc_gen_power_setpoint(pm, nw=n, bounded = false)
+        variable_mc_load_setpoint(pm, nw=n, bounded = false)
+        # Constraints
+        for (i,bus) in ref(pm, :ref_buses, nw=n)
+            @assert bus["bus_type"] == 3
+            constraint_mc_theta_ref(pm, i, nw=n)
+            constraint_mc_voltage_magnitude_only(pm, i, nw=n)
+        end;
+        # gens should be constrained before KCL, or Pd/Qd undefined
+        for id in ids(pm, :gen, nw=n)
+            constraint_mc_gen_setpoint(pm, id, nw=n)
+        end;
+        # loads should be constrained before KCL, or Pd/Qd undefined
+        for id in ids(pm, :load, nw=n)
+            constraint_mc_load_setpoint(pm, id, nw=n)
+        end;
+        for (i,bus) in ref(pm, :bus, nw=n)
+            constraint_mc_load_current_balance(pm, i, nw=n)
+            # PV Bus Constraints
+            if length(ref(pm, :bus_gens, i, nw=n)) > 0 && !(i in ids(pm,:ref_buses, nw=n))
+                # this assumes inactive generators are filtered out of bus_gens
+                @assert bus["bus_type"] == 2
+                constraint_mc_voltage_magnitude_only(pm, i, nw=n)
+                for j in ref(pm, :bus_gens, i, nw=n)
+                    constraint_mc_gen_power_setpoint_real(pm, j, nw=n)
+                end;
+            end;
+        end;
+        for i in ids(pm, :branch, nw=n)
+            constraint_mc_current_from(pm, i, nw=n)
+            constraint_mc_current_to(pm, i, nw=n)
 
-#reference_profiles = read_dublin_data()
-time_series_solution = Dict{Int32,Any}()
-network_model = build_mathematical_network_model(dir,feeder,1,reference_profiles,1.0)
-for time_step in 1:5
-    updated_network_model = update_mathematical_network_model(network_model,time_step,reference_profiles,1.0)
-    result = run_mc_pf(updated_network_model, ACPPowerModel, solver)
-    time_series_solution[time_step] = result
+            constraint_mc_bus_voltage_drop(pm, i, nw=n)
+        end;
+
+        for i in ids(pm, :transformer)
+            constraint_mc_transformer_power(pm, i, nw=n)
+        end;
+    end;
+end;
+
+"Alias to run multinetwork powerflow using run_mc_model() from PowerModelsDistribution"
+function run_mn_mc_pf(data::Union{Dict{String,<:Any},String}, model_type::Type, solver; kwargs...)
+    return run_mc_model(data, model_type, solver, build_mn_mc_pf, multiconductor=true, multinetwork=true, kwargs...)
+end;
+
+"Run time series analysis of the specified feeder in POLA using Dublin data load profiles as reference"
+function time_series_analysis()
+    reference_profiles = read_dublin_data()
+    multinetwork_model = build_mn_mathematical_model(dir,feeder,reference_profiles,48)
+    solver = with_optimizer(Ipopt.Optimizer, print_level = 3, tol=1e-9)
+    return run_mn_mc_pf(multinetwork_model,ACPPowerModel,solver)
+end
+
+"Run Time series analysis by simply rerunning PowerModelsDistribution for each time step "
+function naive_time_series_analysis()
+    reference_profiles = read_dublin_data()
+    multinetwork_model = build_mn_mathematical_model(dir,feeder,reference_profiles,48)
+    solver = with_optimizer(Ipopt.Optimizer, print_level = 1, tol=1e-9)
+    result = Dict{String,Any}()
+    for (n,network) in multinetwork_model["nw"]
+        network["per_unit"] = true
+        result[n] = run_mc_pf(network,ACPPowerModel, solver)
+    end;
+    return result
 end;
